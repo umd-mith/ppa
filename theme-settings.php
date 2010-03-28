6e829c459b11e45a67e27e7692d8dc503873292b
Jarosaw Foksa <jarek_foksa@479726.no-reply.drupal.org>
2010-03-28 18:37:17 +0000

    First commit

---
+++ theme-settings.php
@@ -0,0 +1,21 @@
+<?php
+// $Id$
+
+function corolla_form_system_theme_settings_alter(&$form, $form_state) {
+  // Generate the form using Forms API. http://api.drupal.org/api/7
+  $form['custom'] = array(
+    '#title' => 'Custom theme settings', 
+    '#type' => 'fieldset', 
+  );
+  $form['custom']['color_scheme'] = array(
+    '#title' => 'Color scheme', 
+    '#type' => 'select',
+    '#default_value' => theme_get_setting('color_scheme'),
+    '#options' => array(
+      'green' => 'Green (default)',
+      'blue' => 'Blue',
+      'black' => 'Black',
+    ),
+  );
+}
+
