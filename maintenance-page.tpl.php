81a7f371ea7e033a0e5fb98eeb0eef895935b681
Jeff Burnz <git@adaptivethemes.com>
2010-05-20 04:33:07 +0000

    update maintenance page to match page.tpl.php layout

--- maintenance-page.tpl.php
+++ maintenance-page.tpl.php
@@ -12,22 +12,19 @@
  * @see template_preprocess_maintenance_page()
  */
 ?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
-
 <head>
   <title><?php print $head_title; ?></title>
   <?php print $head; ?>
   <?php print $styles; ?>
   <?php print $scripts; ?>
 </head>
-
 <body class="<?php print $classes; ?>">
-<div id="header-wrapper">
-  <div id="header">
+<div id="page">
 
+  <div id="header">
     <div id="branding" class="clearfix">
       <?php if ($logo): ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
@@ -55,21 +52,19 @@
         </div> <!-- /#name-and-slogan -->
       <?php endif; ?>
     </div> <!-- /#branding -->
-
   </div> <!-- /#header -->
-</div> <!-- /#header-wrapper -->
 
-<div id="main-wrapper">
-  <div id="main">
+  <div id="main-columns"><div id="main-columns-inner" class="clearfix">
+    <div id="main-wrapper"><div id="main">
+      <div id="main-content">
+        <div id="content" class="clearfix">
+          <?php if ($title): ?>
+            <h1 class="page-title"><?php print $title ?></h1>
+          <?php endif; ?>
+          <?php print $content; ?>
+        </div> <!-- /#content -->
+      </div> <!-- /#main-content -->
+    </div></div> <!-- /#main-wrapper /#main -->
+  </div></div> <!-- /#main-columns-inner /#main-columns -->
 
-    <div id="page">
-      <?php if ($title): ?>
-        <h1 class="page-title"><?php print $title ?></h1>
-      <?php endif; ?>
-      
-      <?php print $content; ?>
-    </div> <!-- /#page -->
-
-  </div> <!-- /#main -->
-</div> <!-- /#main-wrapper -->
-
+</div> <!-- /#page -->
