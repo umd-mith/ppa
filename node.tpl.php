86c00a6ed9b0feb15b056f8caaad77e055f11bf0
Jeff Burnz <git@adaptivethemes.com>
2010-05-21 10:07:12 +0000

    fix broken DIV in node tpl, remove some white space from style.css and clean up a few doc blocks

--- node.tpl.php
+++ node.tpl.php
@@ -92,7 +92,7 @@
     <?php endif; ?>
   </div> <!-- /.node-header -->
 
-  <div class="content"<?php print $content_attributes; ?>
+  <div class="content"<?php print $content_attributes; ?>>
     <?php print $user_picture; ?>
     <?php
       // We hide the comments and links now so that we can render them later.
@@ -101,7 +101,7 @@
       print render($content);
     ?>
   </div>
-  
+
   <div class="links-wrapper">
     <?php print render($content['links']); ?>
   </div>
