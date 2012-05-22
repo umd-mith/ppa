46f4f1de140d33523e07a50fb2d5380287d26c2e
Jeff Burnz <jeff@adaptivethemes.com>
2012-05-22 08:03:58 +0200

    remove scripts, change indentation in page tpl, remove position relative from top menu bar

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,149 +1,201 @@
 <?php // Corolla ?>
-<div id="page-wrapper"><div id="page" class="<?php print $classes; ?>">
+<div id="page-wrapper">
+  <div id="page" class="<?php print $classes; ?>">
 
-  <?php if ($menubar = render($page['menu_bar'])): ?>
-    <div id="menu-bar-wrapper"><div class="container clearfix">
-      <?php print $menubar; ?>
-    </div></div>
-  <?php endif; ?>
-
-  <div id="header-wrapper"><div class="container clearfix">
-    <header class="clearfix<?php print $site_logo ? ' with-logo' : ''; ?>" role="banner">
-      <div id="branding">
-
-        <?php if ($site_logo): ?>
-          <div id="logo"><?php print $site_logo; ?></div>
-        <?php endif; ?>
-
-        <?php if ($site_name || $site_slogan): ?>
-          <hgroup<?php if (!$site_slogan && $hide_site_name): ?> class="<?php print $visibility; ?>"<?php endif; ?>>
-            <?php if ($site_name): ?>
-              <h1 id="site-name"<?php if ($hide_site_name): ?> class="<?php print $visibility; ?>"<?php endif; ?>><?php print $site_name; ?></h1>
-            <?php endif; ?>
-            <?php if ($site_slogan): ?>
-              <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
-            <?php endif; ?>
-          </hgroup>
-        <?php endif; ?>
-
+    <?php if ($menubar = render($page['menu_bar'])): ?>
+      <div id="menu-bar-wrapper">
+        <div class="container clearfix">
+          <?php print $menubar; ?>
+        </div>
       </div>
+    <?php endif; ?>
 
-      <?php print render($page['header']); ?>
+    <div id="header-wrapper">
+      <div class="container clearfix">
 
-    </header>
-  </div></div>
+        <header class="clearfix<?php print $site_logo ? ' with-logo' : ''; ?>" role="banner">
 
-  <?php if ($page['three_33_first'] || $page['three_33_second'] || $page['three_33_third']): ?>
-    <div id="top-panels-wrapper"><div class="container clearfix">
-      <div class="three-3x33 at-panel gpanel clearfix">
-        <?php print render($page['three_33_first']); ?>
-        <?php print render($page['three_33_second']); ?>
-        <?php print render($page['three_33_third']); ?>
-      </div>
-    </div></div>
-  <?php endif; ?>
+          <?php if ($site_logo || $site_name || $site_slogan): ?>
+            <div id="branding" class="branding-elements clearfix">
 
-  <?php if ($page['secondary_content']): ?>
-    <div id="secondary-content-wrapper"><div class="container clearfix">
-      <?php print render($page['secondary_content']); ?>
-    </div></div>
-   <?php endif; ?>
-
-  <?php if ($messages || $page['help']): ?>
-    <div id="messages-help-wrapper"><div class="container clearfix">
-      <?php print $messages; ?>
-      <?php print render($page['help']); ?>
-    </div></div>
-  <?php endif; ?>
-
-  <div id="content-wrapper"><div class="container">
-
-    <div id="columns"><div class="columns-inner clearfix">
-      <div id="content-column"><div class="content-inner">
-
-        <?php print render($page['highlighted']); ?>
-
-        <?php $tag = $title ? 'section' : 'div'; ?>
-        <<?php print $tag; ?> id="main-content" role="main">
-
-          <?php if ($primary_local_tasks): ?>
-            <div id="tasks" class="clearfix">
-              <?php if ($primary_local_tasks): ?>
-                <ul class="tabs primary"><?php print render($primary_local_tasks); ?></ul>
+              <?php if ($site_logo): ?>
+                <div id="logo">
+                  <?php print $site_logo; ?>
+                </div>
               <?php endif; ?>
+
+              <?php if ($site_name || $site_slogan): ?>
+                <hgroup id="name-and-slogan"<?php print $hgroup_attributes; ?>>
+
+                  <?php if ($site_name): ?>
+                    <h1 id="site-name"<?php print $site_name_attributes; ?>><?php print $site_name; ?></h1>
+                  <?php endif; ?>
+
+                  <?php if ($site_slogan): ?>
+                    <h2 id="site-slogan"<?php print $site_slogan_attributes; ?>><?php print $site_slogan; ?></h2>
+                  <?php endif; ?>
+
+                </hgroup>
+              <?php endif; ?>
+
             </div>
           <?php endif; ?>
 
-          <div class="content-margin"><div class="content-style">
+          <?php print render($page['header']); ?>
 
-            <?php if ($secondary_local_tasks): ?>
-              <ul class="tabs secondary"><?php print render($secondary_local_tasks); ?></ul>
-            <?php endif; ?>
+        </header>
 
-            <?php if ($breadcrumb): ?>
-              <div id="breadcrumb-wrapper">
-                <section class="breadcrumb clearfix">
-                  <?php print $breadcrumb; ?>
-                </section>
-              </div>
-            <?php endif; ?>
+      </div>
+    </div>
 
-            <?php print render($title_prefix); ?>
-            <?php if ($title && !isset($node)): ?>
-              <header class="clearfix">
-                <h1 id="page-title"><?php print $title; ?></h1>
-              </header>
-            <?php endif; ?>
-            <?php print render($title_suffix); ?>
+    <?php if ($page['three_33_first'] || $page['three_33_second'] || $page['three_33_third']): ?>
+      <div id="top-panels-wrapper">
+        <div class="container clearfix">
 
-            <?php if ($action_links = render($action_links)): ?>
-              <ul class="action-links"><?php print $action_links; ?></ul>
-            <?php endif; ?>
+          <div class="three-3x33 at-panel gpanel clearfix">
+            <?php print render($page['three_33_first']); ?>
+            <?php print render($page['three_33_second']); ?>
+            <?php print render($page['three_33_third']); ?>
+          </div>
 
-          <div id="content"><?php print render($page['content']); ?></div>
-
-          <?php print $feed_icons; ?>
-
-          </div></div>
-        </<?php print $tag; ?>>
-
-        <?php print render($page['content_aside']); ?>
-
-      </div></div>
-
-      <?php print render($page['sidebar_first']); ?>
-      <?php print render($page['sidebar_second']); ?>
-
-    </div></div>
-  </div></div>
-
-  <?php if ($page['tertiary_content']): ?>
-    <div id="tertiary-content-wrapper"><div class="container clearfix">
-      <?php print render($page['tertiary_content']); ?>
-    </div></div>
-  <?php endif; ?>
-
-  <?php if ($page['four_first'] || $page['four_second'] || $page['four_third'] || $page['four_fourth']): ?>
-    <div id="footer-panels-wrapper"><div class="container clearfix">
-      <div class="at-panel gpanel panel-display four-4x25 clearfix">
-        <div class="panel-row row-1 clearfix">
-          <?php print render($page['four_first']); ?>
-          <?php print render($page['four_second']); ?>
-        </div>
-        <div class="panel-row row-2 clearfix">
-          <?php print render($page['four_third']); ?>
-          <?php print render($page['four_fourth']); ?>
         </div>
       </div>
-    </div></div>
-  <?php endif; ?>
+    <?php endif; ?>
 
-  <?php if ($page['footer']): ?>
-    <div id="footer-wrapper"><div class="container clearfix">
-      <footer class="clearfix" role="contentinfo">
-        <?php print render($page['footer']); ?>
-      </footer>
-    </div></div>
-  <?php endif; ?>
+    <?php if ($page['secondary_content']): ?>
+      <div id="secondary-content-wrapper">
+        <div class="container clearfix">
+          <?php print render($page['secondary_content']); ?>
+        </div>
+      </div>
+     <?php endif; ?>
 
-</div></div>
+    <?php if ($messages || $page['help']): ?>
+      <div id="messages-help-wrapper">
+        <div class="container clearfix">
+          <?php print $messages; ?>
+          <?php print render($page['help']); ?>
+        </div>
+      </div>
+    <?php endif; ?>
+
+    <div id="content-wrapper">
+      <div class="container">
+
+        <div id="columns">
+          <div class="columns-inner clearfix">
+
+            <div id="content-column">
+              <div class="content-inner">
+
+                <?php print render($page['highlighted']); ?>
+
+                <<?php print $tag; ?> id="main-content" role="main">
+
+                  <?php if ($primary_local_tasks): ?>
+                    <div id="tasks" class="clearfix">
+
+                      <?php if ($primary_local_tasks): ?>
+                        <ul class="tabs primary">
+                          <?php print render($primary_local_tasks); ?>
+                        </ul>
+                      <?php endif; ?>
+
+                    </div>
+                  <?php endif; ?>
+
+                  <div class="content-margin">
+                    <div class="content-style">
+
+                      <?php if ($secondary_local_tasks): ?>
+                        <ul class="tabs secondary">
+                          <?php print render($secondary_local_tasks); ?>
+                        </ul>
+                      <?php endif; ?>
+
+                      <?php if ($breadcrumb): ?>
+                        <div id="breadcrumb-wrapper">
+                          <?php print $breadcrumb; ?>
+                        </div>
+                      <?php endif; ?>
+
+                      <?php print render($title_prefix); ?>
+
+                      <?php if ($title && !isset($node)): ?>
+                        <header class="clearfix">
+                          <h1 id="page-title">
+                            <?php print $title; ?>
+                          </h1>
+                        </header>
+                      <?php endif; ?>
+
+                      <?php print render($title_suffix); ?>
+
+                      <?php if ($action_links = render($action_links)): ?>
+                        <ul class="action-links">
+                          <?php print $action_links; ?>
+                        </ul>
+                      <?php endif; ?>
+
+                      <div id="content">
+                        <?php print render($page['content']); ?>
+                      </div>
+
+                      <?php print $feed_icons; ?>
+
+                    </div>
+                  </div>
+
+                </<?php print $tag; // end main content ?>>
+
+                <?php print render($page['content_aside']); ?>
+
+              </div>
+            </div>
+
+            <?php print render($page['sidebar_first']); ?>
+            <?php print render($page['sidebar_second']); ?>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <?php if ($page['tertiary_content']): ?>
+      <div id="tertiary-content-wrapper">
+        <div class="container clearfix">
+          <?php print render($page['tertiary_content']); ?>
+        </div>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($page['four_first'] || $page['four_second'] || $page['four_third'] || $page['four_fourth']): ?>
+      <div id="footer-panels-wrapper">
+        <div class="container clearfix">
+          <div class="at-panel gpanel panel-display four-4x25 clearfix">
+            <div class="panel-row row-1 clearfix">
+              <?php print render($page['four_first']); ?>
+              <?php print render($page['four_second']); ?>
+            </div>
+            <div class="panel-row row-2 clearfix">
+              <?php print render($page['four_third']); ?>
+              <?php print render($page['four_fourth']); ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($page['footer']): ?>
+      <div id="footer-wrapper">
+        <div class="container clearfix">
+          <footer class="clearfix" role="contentinfo">
+            <?php print render($page['footer']); ?>
+          </footer>
+        </div>
+      </div>
+    <?php endif; ?>
+
+  </div>
+</div>
